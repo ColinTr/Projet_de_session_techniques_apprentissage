5a70164802f54adcc989c86967be75a34ec77299
colin <56585263+ColinTr@users.noreply.github.com>
2020-11-15 04:18:29 -0800

    Added .gitkeep to data/processed folder

---
+++ src/models/perceptron.py
@@ -0,0 +1,20 @@
+class Perceptron:
+    def __init__(self, x_train, t_train, x_test, t_test, lamb, max_iterations):
+        self.x_train = x_train
+        self.t_train = t_train
+        self.x_test = x_test
+        self.t_test = t_test
+
+        self.w = None
+        self.w_0 = None
+        self.lamb = lamb
+        self.max_iterations = max_iterations
+
+    def training(self):
+        return
+
+    def prediction(self):
+        return
+
+    def cross_validation(self):
+        return
